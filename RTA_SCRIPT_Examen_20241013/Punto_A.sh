45350651fbf73beed9bdcb9a0492739640585ab6
lourdes-privitera <vagrant@LPrivitera>
2024-10-13 23:44:41 +0000

    asd

---
+++ RTA_SCRIPT_Examen_20241013/Punto_A.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sudo mkdir -p /Examenes-UTN/{alumno_1/{parcial_1,parcial_2,parcial_3},alumno_2/{parcial_1,parcial_2,parcial_3},alumno_3/{parcial_1,parcial_2,parcial_3},profesores}
+
+
+echo "Cree carpetas"
